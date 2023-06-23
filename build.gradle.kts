30cfe96c6e2d1622aa37aedeee17b11bf6812612
Vincent Nguyen <vincentnguyen88@hotmail.com>
2023-06-23 02:27:19 -0700

    - added lombok to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,22 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "com.lighttech"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    gradlePluginPortal()
+    google()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
 }
 
 tasks.test {
